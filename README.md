853eeae8f6f355c521ba9ea304a47f564a8728c5
bean <bean@Beans-Macbook.local>
2016-03-16 22:42:22 +0700

    Update

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ A tool to generate password from words that user provide.
 By typing `python bpwd.py -h usage`, you can list all arguments
 
 ```
-usage: bpwd.py [-h] [-A ADD] [-a APPEND] [-T TRANSFORM] [-m MAXITEM]
+usage: bpwd.py [-h] [-A ADD] [-T TRANSFORM] [-m MAXITEM]
                [-l MAXLEN] -lst LISTWORD [-ext EXTENDWORD] [-o OUTPUT]
 
 Password Generator by sting8k
@@ -19,11 +19,8 @@ optional arguments:
   -h, --help            show this help message and exit
   -A ADD, --add ADD     Add words to a wordlist file named by -lst, split by
                         [space]
-  -a APPEND, --append APPEND
-                        Append words to an existing wordlist file called by
-                        -lst, split by [space]
   -T TRANSFORM, --transform TRANSFORM
-                        Auto generate Uppercase to words in -A or -a argument
+                        Auto generate Uppercase to words in -A argument
   -m MAXITEM, --maxitem MAXITEM
                         Set Max items to combine
   -l MAXLEN, --maxlen MAXLEN
@@ -45,7 +42,9 @@ Init the first wordlist with some words
 [*] Estimated ~1956 words in list!
 [*] Generated 1956 words in 0.00113391876221
 [*] 1956 words were written to output.txt
+
 ```
+
 Then we got a file named input.txt
 
 ```
@@ -75,13 +74,14 @@ john01281992
 
 Then if you want to add more words to this input.txt:
 ```
-> python bpwd.py -a "cena sayian" -lst input.txt
+> python bpwd.py -A "cena sayian" -lst input.txt
 [*] Estimated ~9864100 words in list!
 [*] Generated 9864100 words in 6.22505402565
 [*] 9864100 words were written to output.txt
+
 ```
 
-We got new input.txt
+We got this in input.txt
 
 ```
 john
@@ -125,6 +125,7 @@ $
 > python bpwd.py -ext ext.txt -lst input.txt
 [*] Estimated ~16926797485 words in list!
 ...
+
 ```
 
 
--- bpwd.py
+++ bpwd.py
@@ -3,62 +3,95 @@ import math
 import argparse
 import traceback
 import time
-
+import os
 from datetime import date
 
-cur_year = date.today().year
+class password_generator:
+	def __init__(self, inputWords, maxinputwords, maxlen):
+		self.inputWords = inputWords
+		self.maxinputwords = maxinputwords
+		self.maxlen = maxlen
 
-argwtf = {}
+		if maxinputwords==0:
+			self.maxinputwords = len(self.inputWords)
+		if maxlen!=0:
+			tmp = self.__maxinputWords_by_len()
+			if tmp < maxinputwords:
+				self.maxinputwords = tmp
 
+	def npr(self, n, r):
+		return (stirling(n)/stirling(n-r) if n > 20 else
+			math.factorial(n)/math.factorial(n-r))
 
-def npr(n,r):
-	return (stirling(n)/stirling(n-r) if n>20 else
-		math.factorial(n)/math.factorial(n-r))
+	@staticmethod
+	def yob_filter(s):
+		try:
+			s = int(s)
+			if s>=1900 and s<=date.today().year:
+				return str(s)[2:4]
+			return False
+		except Exception, e:
+			return False
 
-def maxitem_by_len(lstword):
-	sort_lst = sorted(lstword, key=len)
-	mlen = 0
-	tmp = 0
-	for word in lstword:
-		mlen += len(word)
-		if not mlen>argwtf['maxlen']:
-			tmp += 1
-		else:
-			break
-	return tmp
+	@staticmethod
+	def clear_emptyword(words):
+		for i in range(0,len(words)):
+			if words[i] == "":
+				del words[i]
+		return words
 
-def counting(lstword):
-	count = 0
-	maxitem = argwtf['maxitem']
-	if maxitem==0:
-		maxitem = len(lstword)
-
-	for i in range(1,maxitem+1):
-		count+=npr(len(lstword),i)
-	return count
+	def output_counter(self):
+		count = 0
+		for i in range(1, self.maxinputwords+1):
+			count += self.npr(len(self.inputWords),i)
+		return count
 	
-def generate(lstword):
-	res = []
-	maxitem = argwtf['maxitem']
-	if maxitem==0:
-		maxitem = len(lstword)
+	def output_generator(self):
+		res = []
+		for i in range(1, self.maxinputwords+1):
+			for x in itertools.combinations(self.inputWords,i):
+				if not (len(str(''.join(x))) <= self.maxlen or self.maxlen == 0):
+					continue
+				for y in itertools.permutations(x,i):
+					res.append(''.join(y))
+		return res
 
-	for i in range(1,maxitem+1):
-		for x in itertools.combinations(lstword,i):
-			if not (len(str(''.join(x)))<=argwtf['maxlen'] or argwtf['maxlen']==0):
-				continue
-			for y in itertools.permutations(x,i):
-				res.append(''.join(y))
+	@staticmethod
+	def newword_filter(words, transform):
+		tmp = words
+		for word in tmp:
+			if transform == 1:
+				if word.title() != word:
+					words.append(word.title())
+			y = password_generator.yob_filter(word)
+			if y!=False:
+				words.append(y)
+		print words
+		return password_generator.clear_emptyword(words)
 
-	return res
+	def __maxinputWords_by_len(self):
+		sorted_inputWords = sorted(self.inputWords, key=len)
+		wlen = 0
+		count = 0
+		for word in sorted_inputWords:
+			wlen += len(word)
+			if not wlen > self.maxlen:
+				count += 1
+			else:
+				break
+		return count
 
-def clean_lstword(words):
-	for i in range(0,len(words)):
-		if words[i] == "":
-			del words[i]
-	return words
 
-def chk_file(p_file):
+def write_file(p_file, data):
+	try:
+		foutput = open(p_file ,"w")
+		foutput.write(data)
+		foutput.close()
+	except Exception, e:
+		print e
+		return False
+
+def read_file(p_file):
 	try:
 		finput = open(p_file,"r")
 		input = finput.read()
@@ -67,101 +100,62 @@ def chk_file(p_file):
 	except Exception, e:
 		print e
 		return False
-		
-def render_append():
-	try:
-		check = chk_file(argwtf['listword'])
-		if check!=False:
-			old_words = clean_lstword(check)
-			new_words = clean_lstword(argwtf['append'].split())
-			return old_words+new_word(new_words,argwtf['transform'])
-		print "Cannot open file", argwtf['listword']
-		return False
-	except Exception, e:
-		print(traceback.format_exc())
-		return False
-		
-def yob(s):
-	try:
-		s = int(s)
-		if s>=1900 and s<=cur_year:
-			return str(s)[2:4]
-		return False
-	except Exception, e:
-		return False
-
-def new_word(words,transform):
-	newwd = words
-	if transform == 1:
-		for word in newwd:
-			if word.title() != word:
-				newwd.append(word.title())
-			y = yob(word)
-			if y!=False:
-				newwd.append(y)
-	return newwd
 	
-def output(words,dtime):
+def output(p_file, words, dtime):
 	try:
 		print "[*] Generated "+str(len(words))+" words in "+str(dtime)
 		data = '\n'.join(words)
-		foutput = open(argwtf['output'],"w")
-		foutput.write(data)
-		foutput.close()
-		print "[*] "+str(len(words))+" words were written to "+argwtf['output']
+		write_file(p_file, data)
+		print "[*] "+str(len(words))+" words were written to " + p_file
 	except Exception, e:
 		print(traceback.format_exc())
 
-parser = argparse.ArgumentParser(description='Password Generator by sting8k')
-parser.add_argument('-A','--add', help='Add words to a wordlist file named by -lst, split by [space]', default="")
-parser.add_argument('-a','--append', help='Append words to an existing wordlist file called by -lst, split by [space]', default="")
-parser.add_argument('-T','--transform', help='Auto generate Uppercase to words in -A or -a argument', default=1)
-parser.add_argument('-m','--maxitem', help='Set Max items to combine', default=0)
-parser.add_argument('-l','--maxlen', help='Set Max length of a word', default=0)
-parser.add_argument('-lst','--listword', help='Wordlist to combine', required=True, default="")
-parser.add_argument('-ext','--extendword', help='An extend wordlist', default="0")
-parser.add_argument('-o','--output', help='Output file', default="output.txt")
+def main():
+	global args
 
-args = vars(parser.parse_args())
-argwtf = args
-argwtf['maxitem'] = int(argwtf['maxitem'])
-argwtf['maxlen'] = int(argwtf['maxlen'])
-argwtf['transform'] = int(argwtf['transform'])
+	parser = argparse.ArgumentParser(description='Password Generator by sting8k')
+	parser.add_argument('-A','--add', help='Add words to a wordlist file named by -lst, split by [space]', default="")
+	parser.add_argument('-T','--transform', help='Auto generate Uppercase to words in -A or -a argument', default=1)
+	parser.add_argument('-m','--maxinputwords', help='Set Max input words to combine', default=0)
+	parser.add_argument('-l','--maxlen', help='Set Max length of a word', default=0)
+	parser.add_argument('-lst','--listword', help='Wordlist to combine', required=True, default="")
+	parser.add_argument('-ext','--extendword', help='An extend wordlist', default="0")
+	parser.add_argument('-o','--output', help='Output file', default="output.txt")
+	args = vars(parser.parse_args())
 
+	args['maxinputwords'] = int(args['maxinputwords'])
+	args['maxlen'] = int(args['maxlen'])
+	args['transform'] = int(args['transform'])
 
-if argwtf['add'] != "":
-	rs = new_word(argwtf['add'].split(),argwtf['transform'])
-	data = '\n'.join(rs)
-	f= open(argwtf['listword'],"w")
-	f.write(data)
-	f.close()
+	inputWords = []
 
-if argwtf['append'] != "":
-	rs = render_append()
-	if rs!=False:
-		data = '\n'.join(rs)
-		f= open(argwtf['listword'],"w")
-		f.write(data)
-		f.close()
+	if args['listword'] != "":
+		if args['add'] != "":
+			inputWords = password_generator.newword_filter(args['add'].split(), args['transform'])
+		if os.path.isfile(args['listword']):
+			inp = read_file(args['listword'])
+			if inp!= False:
+				old_words = password_generator.clear_emptyword(inp)
+				inputWords = old_words + inputWords
+			else:
+				print "Cannot read file", args['listword']
+			
+		data = '\n'.join(inputWords)
+		write_file(args['listword'], data)
+			
+	if args['extendword'] != "0":
+		ext = read_file(args['extendword'])
+		if ext != False:
+			inputWords += ext
 
-if chk_file(argwtf['listword']) != False:
-	rs = chk_file(argwtf['listword'])
-	if argwtf['extendword'] != "0":
-		if chk_file(argwtf['extendword']) != False:
-			rs += chk_file(argwtf['extendword'])
+	PG = password_generator(inputWords, args['maxinputwords'], args['maxlen'])
 
-	if argwtf['maxlen']!=0:
-		tmp = maxitem_by_len(rs)
-		if tmp<argwtf['maxitem']:
-			argwtf['maxitem']=tmp
-
-	print "[*] Estimated ~"+str(counting(rs))+" words in list!"
-
+	print "[*] Estimated ~"+str(PG.output_counter())+" words in list!"
 	start = time.time()
-	op = generate(rs)
+	op = PG.output_generator()
 	end = time.time()
 	duration = end - start
-	output(op,duration)
+	output(args['output'], op, duration)
 	
-	
-
+if __name__ == "__main__":
+	main()
